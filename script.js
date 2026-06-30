88f6cf74d86a5e68e1846c82de764027d1c771ed
Davit <davit.ners02@gmail.com>
2026-06-30 16:04:35 +0200

    Exo IMC

--- script.js
+++ script.js
@@ -11,3 +11,42 @@ btn1.addEventListener('click', () => {
     // res1.textContent = "Bonjour, je m'appelle " + name + ", et j'ai " + age + ' ans.';
     res1.textContent = `Bonjour, je m'appelle ${name}, et j'ai ${age} ans`;
 });
+
+
+
+// Exercice IMC
+
+// Recuperation des elements HTML
+const inputTaille = document.getElementById('taille');
+const inputPoids = document.getElementById('poids');
+const imcBtn = document.getElementById('imcBtn');
+const imcRes = document.getElementById('resultat');
+const imcStatus = document.getElementById('status');
+
+imcBtn.addEventListener('click', () => {
+    const tailleValue = inputTaille.valueAsNumber;
+    const poidsValue = inputPoids.valueAsNumber;
+
+    if (!tailleValue || !poidsValue) {
+        imcRes.textContent = 'Veuillez remplir tout les champs';
+        return;
+    }
+
+    const tailleEnMetres = tailleValue / 100;
+
+    const resultatIMC = (poidsValue / (tailleEnMetres ** 2)).toFixed(2);
+
+    if (resultatIMC < 18.5) {
+        imcStatus.textContent = 'Vous êtes en sous poids';
+    } else if (resultatIMC < 25) {
+        imcStatus.textContent = "Poids santé";
+    } else if (resultatIMC < 30) {
+        imcStatus.textContent = 'Surpoids';
+    } else {
+        imcStatus.textContent = 'Obesité';
+    }
+
+    imcRes.textContent = `Votre IMC est de ${resultatIMC}`;
+
+
+})
